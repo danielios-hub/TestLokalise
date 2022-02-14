cb536ba55ca1a4849be1333a4dd6a2055850c328
danielios-hub <daniel.ios.dgp@gmail.com>
2022-02-14 19:43:33 +0100

    Add localization

--- TestLokalise/ViewController.swift
+++ TestLokalise/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var textLabel: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textLabel?.text = NSLocalizedString("INITIAL_TEXT", tableName: "Test", bundle: Bundle(for: ViewController.self), comment: "")
     }
 
 
