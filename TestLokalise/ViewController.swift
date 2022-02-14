5d7d4f392675e138a96a62924c2883b6ecdee44b
danielios-hub <daniel.ios.dgp@gmail.com>
2022-02-14 19:27:45 +0100

    Initial Commit

---
+++ TestLokalise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestLokalise
+//
+//  Created by Daniel Gallego Peralta on 14/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
